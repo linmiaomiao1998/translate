880587e0f69541e7d3684fc6a5a05fab110ee8bf
linmiaomiao1998 <994717247@qq.com>
2020-12-31 15:38:23 +0800

    Initial Commit

---
+++ utils/api.js
@@ -0,0 +1,36 @@
+import md5 from './md5.min.js'
+
+const appid = '20180802000191219'
+const key = '02LMiYALQE2vR4q0kaef'
+
+function translate(q, { from = 'auto', to = 'auto' } = { from: 'auto', to: 'auto' }) {
+  return new Promise((resolve, reject) => {
+    let salt = Date.now()
+    let sign = md5(`${appid}${q}${salt}${key}`)
+    wx.request({
+      url: 'https://fanyi-api.baidu.com/api/trans/vip/translate',
+      data: {
+        q,
+        from,
+        to,
+        appid,
+        salt,
+        sign
+      },
+      success(res) {
+        if (res.data && res.data.trans_result) {
+          resolve(res.data)
+        }
+      },
+      fail() {
+        reject({ status: 'error', msg: '翻译失败' })
+        wx.showToast({
+          title: '网络异常',
+          icon: 'none',
+          duration: 3000
+        })
+      }
+    })
+  })
+}
+module.exports.translate = translate
